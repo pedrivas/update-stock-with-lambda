d064b6454337eb7176d896e06e4398ab3fb0c286
Drivas <drivaspedro@gmail.com>
2021-07-22 22:04:14 -0300

    method to update products with the woocommerce API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-const AddItemsOnDynamoDB = require('./utils/AddItemsOnDynamoDB');
 const UpdateStock = require('./utils/UpdateStock');
 
-lambda();
+exports.handler = function(event, context, callback) {
+  lambda();
+}
 
-//exports.handler = function(event, context, callback) {
 async function lambda() {
   
   const updateStock = new UpdateStock();
-  // const addItemsOnDynamoDB = new AddItemsOnDynamoDB();
-
-  // await addItemsOnDynamoDB.addItemsToDynamoDB();
-  // await updateStock.getWorkbookFromS3(true);
-
-  await updateStock.queryDDB();
+  await updateStock.addItemsToDynamoDB();
   
 };
